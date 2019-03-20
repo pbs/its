1ac18c6a7e510231d1ff9f6f866d0037846d0336
cmac1000 <cmac1000@gmail.com>
2019-03-20 09:54:39 -0400

    OCTO-9173 preserve alpha channel (#175)

--- its/normalize.py
+++ its/normalize.py
@@ -4,12 +4,16 @@ from PIL import Image, ImageCms
 
 
 def normalize(image: Image) -> Image:
+    output_mode = "RGB"
+    # if the image has an alpha channel, preserve it
+    if image.mode in ("RGBA", "LA"):
+        output_mode = "RGBA"
     if "icc_profile" in image.info:
         fmt = image.format
         input_icc_profile = io.BytesIO(image.info["icc_profile"])
         output_icc_profile = ImageCms.createProfile("sRGB")
         image = ImageCms.profileToProfile(
-            image, input_icc_profile, output_icc_profile, outputMode="RGB"
+            image, input_icc_profile, output_icc_profile, outputMode=output_mode
         )
         image.format = fmt
     return image
