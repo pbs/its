4c57e90fbdeefc4420d0b29f5c1adbf68083e174
Andrei Boleac <andrei.boleac@3pillarglobal.com>
2020-06-22 15:50:56 +0300

    updated outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "aws_ecs_task_definition.web.arn" {
+output "aws_ecs_task_definition" {
   value = "${aws_ecs_task_definition.web.arn}"
 }
 
-output "aws_ecs_service.web.id" {
+output "aws_ecs_service" {
   value = "${aws_ecs_service.web.id}"
 }
